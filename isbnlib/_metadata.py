17c0300378f21839ec8b6fa07de280e9fbc66be2
Alexandre Conde <alexandreconde@hotmail.com>
2014-07-03 11:45:56 +0100

    move isbn validation from 'meta' to '_metadata.query'

--- isbnlib/_metadata.py
+++ isbnlib/_metadata.py
@@ -2,12 +2,16 @@
 """Query providers for metadata."""
 
 from .registry import services
+from ._core import EAN13
 from ._exceptions import NotRecognizedServiceError
 
 
 def query(isbn, service='default', cache='UNDEFINED'):
     """Query worldcat.org, Google Books (JSON API), ... for metadata."""
     from .registry import metadata_cache           # <-- dinamic now!
+    isbn = EAN13(isbn)
+    if not isbn:
+        return
     if cache == 'UNDEFINED':
         cache = metadata_cache
     if service != 'default' and service not in services:
