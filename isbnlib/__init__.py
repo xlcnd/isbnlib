893831bc4826b764e27e2ac5c1f11f5bb076349d
Alexandre Conde <alexandreconde@hotmail.com>
2014-07-16 12:18:35 +0100

    goom alias on import

--- isbnlib/__init__.py
+++ isbnlib/__init__.py
@@ -37,9 +37,8 @@ from ._core import (is_isbn10, is_isbn13, to_isbn10, to_isbn13, clean,
                     canonical, notisbn, get_isbnlike, get_canonical_isbn,
                     EAN13)
 from ._ext import (mask, meta, info, editions, isbn_from_words, doi, ren)
-from ._goom import query as goomq
+from ._goom import query as goom
 
 # alias
 ISBN13 = EAN13
 ISBNToolsException = ISBNLibException
-goom = goomq
