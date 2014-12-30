82ac73c21a191446211e0e20973136362ef64683
Alexandre Conde <alexandreconde@hotmail.com>
2014-12-30 16:01:08 +0000

    comments

--- isbnlib/_core.py
+++ isbnlib/_core.py
@@ -17,7 +17,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-"""isbntools main file.
+"""isbnlib main file.
 
 Tools for extracting, cleaning, transforming and validating ISBN ids.
 """
