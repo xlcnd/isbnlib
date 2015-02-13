e40bae9907b79346b7fb49f561d439fe78fd8f02
Alexandre Conde <alexandreconde@hotmail.com>
2015-02-13 11:33:02 +0000

    add 'write to file' to sprint

--- isbnlib/dev/_helpers.py
+++ isbnlib/dev/_helpers.py
@@ -14,7 +14,7 @@ PY2 = sys.version < '3'
 EOL = '\r\n' if WINDOWS and not PY2 else '\n'
 
 
-def sprint(content):    # pragma: no cover
+def sprint(content, filep=None, mode='w'):    # pragma: no cover
     """Smart print function so that redirection works (see issue 75).
 
     The print function doesn't work well with redirection
@@ -22,10 +22,15 @@ def sprint(content):    # pragma: no cover
     """
     s = content + EOL
     buf = s.encode("utf-8")
+    if filep:
+        stdout = sys.stdout
+        sys.stdout = open(filep, mode)
     if PY2:
         sys.stdout.write(buf)
     else:
         sys.stdout.buffer.write(buf)
+    if filep:
+        sys.stdout = stdout
 
 
 def fake_isbn(title, author='unkown', publisher='unkown', sid=1):
