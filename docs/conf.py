76dcce6384e0fedfab9f5700368bf4200f02987e
Alexandre Conde <alexandreconde@hotmail.com>
2015-02-27 11:01:37 +0000

    update main version

--- docs/conf.py
+++ docs/conf.py
@@ -52,7 +52,7 @@ copyright = u'2014, Alexandre Conde'
 # built documents.
 #
 # The short X.Y version.
-version = '3.4'
+version = '3.5'
 # The full version, including alpha/beta/rc tags.
 release = '3.5.1'
 
