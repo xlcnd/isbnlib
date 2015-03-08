5de47d4bb80538faa5ea3b32f1b45d7753672176
Alexandre Conde <alexandreconde@hotmail.com>
2015-03-08 12:29:48 +0000

    fix cover response

--- isbnlib/test/test_ext.py
+++ isbnlib/test/test_ext.py
@@ -51,9 +51,9 @@ def test_desc():
 
 
 def test_cover():
-    assert_equals(cover('9780156001311')[:13], '9780156001311')  # <-- no size 2 (in most areas) but size 1
-    assert_equals(cover('9780000000000'), None)                  # <-- invalid ISBN
-    assert_equals(cover('9781408835029')[:13], '9781408835029')
-    assert_equals(cover('9789727576807'), None)                  # <-- no image of any size
-    
-    
+    assert_equals(cover('9780156001311')[1][:13], '9780156001311')  # <-- no size 2 (in most areas) but size 1
+    assert_equals(cover('9780000000000'), None)                     # <-- invalid ISBN
+    assert_equals(cover('9781408835029')[1][:13], '9781408835029')
+    assert_equals(cover('9789727576807')[1], None)                  # <-- no image of any size
+
+
