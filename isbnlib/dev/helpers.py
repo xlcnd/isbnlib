fafa7f1c0302c1285236d5fd446ffcbc9553b831
Alexandre Conde <alexandreconde@hotmail.com>
2015-03-12 18:49:53 +0000

    del console

--- isbnlib/dev/helpers.py
+++ isbnlib/dev/helpers.py
@@ -11,11 +11,9 @@ from ._helpers import unicode_to_utf8tex as to_utf8tex
 from ._helpers import (cutoff_tokens, fake_isbn, in_virtual, last_first,
                        normalize_space, parse_placeholders)
 from ._shelvecache import ShelveCache
-from ._console import (reset_msconsole, set_msconsole,
-                       set_consolefont, set_codepage, uprint)
+
 
 __all__ = ['CoversCache', 'File', 'IMCache', 'ShelveCache', 'cutoff_tokens', 'cwdfiles',
            'fmtbib', 'fmts', 'in_virtual', 'last_first', 'normalize_space',
-           'parse_placeholders', 'to_utf8tex', 'uprint', 'fake_isbn',
-           'reset_msconsole', 'set_msconsole', 'set_consolefont', 'set_codepage'
+           'parse_placeholders', 'to_utf8tex', 'fake_isbn'
            ]
