74c5156b063526bba1612dcfa7d40f47303f241f
Alexandre Conde <alexandreconde@hotmail.com>
2015-04-20 14:08:37 +0100

    split test_vias into two tests

--- isbnlib/test/test_vias.py
+++ isbnlib/test/test_vias.py
@@ -15,7 +15,7 @@ from nose.tools import assert_equals
 WINDOWS = os.name == 'nt'
 
 
-def test_vias():
+def test_vias_modes():
     """Test 'vias' several modes of operation."""
     if WINDOWS:
         # appveyor doesn't allow!
@@ -24,6 +24,8 @@ def test_vias():
     assert_equals(len(repr(merge.query('9780321534965', 'multi'))) > 170, True)
     assert_equals(len(repr(merge.query('9780321534965', 'serial'))) > 170, True)
 
+def test_vias_cache_cleanning():
+    """Test 'vias' cache cleanning for serial."""
     # test if the secondary cache (cache in vias) does clears... sequentially
     assert_equals(len(repr(merge.query('9781484206546', 'serial'))) < 20, True)  # NO METADATA
     assert_equals(len(repr(merge.query('9780321534965', 'serial'))) > 170, True)
