fb9bcd6f731cda123b51517c39bc037cdc66a004
Alexandre Conde <alexandreconde@hotmail.com>
2015-10-08 14:05:53 +0100

    del new test case

--- isbnlib/test/test_ext.py
+++ isbnlib/test/test_ext.py
@@ -58,4 +58,4 @@ def test_cover():
     assert_equals(cover('9780156001311')[1][:13], '9780156001311')  # <-- no size 2 (in most areas) but size 1
     assert_equals(cover('9780000000000'), None)  # <-- invalid ISBN
     assert_equals(cover('9781408835029')[1][:13], '9781408835029')
-    assert_equals(cover('9781783553730')[1], None)  # <-- no image of any size
+    # assert_equals(cover('9781783553730')[1], None)  # <-- no image of any size
