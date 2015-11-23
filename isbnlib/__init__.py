9f722d6200a336a3fdf23dd37e92299aedcddc9a
Alexandre Conde <alexandreconde@hotmail.com>
2015-11-23 13:07:43 +0000

    add GTIN-13 alias

--- isbnlib/__init__.py
+++ isbnlib/__init__.py
@@ -28,8 +28,8 @@ from . import config  # <-- first import
 
 # main modules
 from ._core import (is_isbn10, is_isbn13, to_isbn10, to_isbn13, clean,
-                    canonical, notisbn, get_isbnlike, get_canonical_isbn, EAN13
-                    )
+                    canonical, notisbn, get_isbnlike, get_canonical_isbn,
+                    GTIN13, EAN13)
 from ._ext import (cover, desc, mask, meta, info, editions, isbn_from_words,
                    doi, ren)
 from ._goom import query as goom
