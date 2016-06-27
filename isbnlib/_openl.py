f1637bae57b8441e10ac6aaba3690aaf8776d181
Alexandre Conde <alexandreconde@hotmail.com>
2016-06-27 13:51:37 +0100

    qa -- fmt

--- isbnlib/_openl.py
+++ isbnlib/_openl.py
@@ -27,8 +27,8 @@ def _mapper(isbn, records):
         canonical['Authors'] = [
             a['name'] for a in records.get('authors', ({'name': u('')}, ))
         ]
-        canonical['Publisher'] = records.get('publishers', [{'name': u('')},
-                                                            ])[0]['name']
+        canonical['Publisher'] = records.get('publishers',
+                                             [{'name': u('')}, ])[0]['name']
         canonical['Year'] = u('')
         strdate = records.get('publish_date', u(''))
         if strdate:  # pragma: no cover
