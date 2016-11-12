75e1a2813498108695557873acb2c0cfc73c3359
Alexandre Conde <alexandreconde@hotmail.com>
2016-11-12 14:23:12 +0000

    init v3.6.5

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     author='xlcnd',
     author_email='xlcnd@outlook.com',
     url='https://github.com/xlcnd/isbnlib',
-    download_url='https://github.com/xlcnd/isbnlib/archive/v3.6.4.zip',
+    download_url='https://github.com/xlcnd/isbnlib/archive/v3.6.5.zip',
     packages=['isbnlib',
               'isbnlib/dev',
               'isbnlib/_data',
