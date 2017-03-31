d9c150fa24995fb019ee816452bb0fc73d799ee6
Alexandre Conde <alexandreconde@hotmail.com>
2017-03-31 12:45:24 +0100

    expose check_digit... functions (close #36)

--- isbnlib/__init__.py
+++ isbnlib/__init__.py
@@ -45,13 +45,14 @@ ean13 = EAN13
 
 # dunders
 
-__all__ = (
-    'is_isbn10', 'is_isbn13', 'clean', 'mask', 'info', 'meta', 'to_isbn10',
-    'to_isbn13', 'get_isbnlike', 'notisbn', 'ean13', 'EAN13', 'cover', 'desc',
-    'canonical', 'get_canonical_isbn', 'editions', 'isbn_from_words',
-    'quiet_errors', 'config', '__version__', '__support__', 'doi', 'ren',
-    'ISBN13', 'ISBNLibException', 'NotRecognizedServiceError',
-    'NotValidISBNError', 'PluginNotLoadedError', 'goom', 'doi2tex', 'RDDATE')
+__all__ = ('is_isbn10', 'is_isbn13', 'clean', 'check_digit10', 'check_digit13',
+           'mask', 'info', 'meta', 'to_isbn10', 'to_isbn13', 'get_isbnlike',
+           'notisbn', 'ean13', 'EAN13', 'cover', 'desc', 'canonical',
+           'get_canonical_isbn', 'editions', 'isbn_from_words', 'quiet_errors',
+           'config', '__version__', '__support__', 'doi', 'ren', 'ISBN13',
+           'GTIN13', 'ISBNLibException', 'NotRecognizedServiceError',
+           'NotValidISBNError', 'PluginNotLoadedError', 'goom', 'doi2tex',
+           'RDDATE')
 
 __version__ = '3.6.8'  # <-- literal IDs
 __support__ = 'py26, py27, py33, py34, py35, pypy, pypy3'  # <-- literal IDs
