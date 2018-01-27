3e40d2ca1cfc97c1c53571bf70dd705889bc131c
Alexandre Conde <alexandreconde@hotmail.com>
2018-01-27 08:54:46 +0000

    qa -- clean

--- isbnlib/_metadata.py
+++ isbnlib/_metadata.py
@@ -8,23 +8,25 @@ from .registry import services
 
 def query(isbn, service='default', cache='default'):
     """Query worldcat.org, Google Books (JSON API), ... for metadata."""
-    from .registry import metadata_cache  # <-- dinamic now!
-    ean = EAN13(isbn)  # <-- XXX maybe this is too strict?
+    # validate inputs
+    ean = EAN13(isbn)
     if not ean:
         raise NotValidISBNError(isbn)
     isbn = ean
     if service != 'default' and service not in services:  # pragma: no cover
         raise NotRecognizedServiceError(service)
+    # set cache and get metadata
     if cache is None:  # pragma: no cover
         return services[service](isbn)
     if cache == 'default':
+        from .registry import metadata_cache
         cache = metadata_cache
     key = isbn + service
     try:
         if cache[key]:
             return cache[key]
         else:  # pragma: no cover
-            raise  # <-- IMPORTANT: usually the caches don't return error!
+            raise  # <-- IMPORTANT: "caches don't return error"!
     except:
         meta = services[service](isbn)
         if meta:
