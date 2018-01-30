151767a1421ff92170d9e9310a722e974aea36eb
Alexandre Conde <alexandreconde@hotmail.com>
2018-01-30 11:28:31 +0000

    use vias

--- isbnlib/_editions.py
+++ isbnlib/_editions.py
@@ -4,6 +4,7 @@
 import logging
 
 from ._core import EAN13
+from .dev import vias
 from ._exceptions import NotRecognizedServiceError, NotValidISBNError
 from ._openled import query as oed
 from ._thinged import query as ted
@@ -31,21 +32,16 @@ def fake_provider_any(isbn):
 
 def fake_provider_merge(isbn):
     """Fake provider 'merge' service."""
-    data = []
     try:  # pragma: no cover
-        wdata = wed(isbn)
-        if not wdata:
-            wdata = []
-        odata = oed(isbn)
-        if not odata:
-            odata = []
-        tdata = ted(isbn)
-        if not tdata:
-            tdata = []
+        named_tasks = (('wcat', wed), ('openl', oed), ('thingl', ted))
+        results = vias.multi(named_tasks, isbn)
+        wdata = results.get('wed', [])
+        odata = results.get('openl', [])
+        tdata = results.get('thingl', [])
         data = list(set(wdata + odata + tdata))
         return data
     except:  # pragma: no cover
-        return data
+        return []
 
 
 def editions(isbn, service='merge'):
