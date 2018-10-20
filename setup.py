32ae68c9edc55aedbee3b2952c83f188517ef478
Alexandre Conde <alexandreconde@hotmail.com>
2018-10-20 15:36:22 +0100

    parametrize setup.py

--- setup.py
+++ setup.py
@@ -30,22 +30,40 @@ PROJECT_AUTHOR = 'Alexandre Lima Conde'
 PROJECT_COPYRIGHT = ' 2014-{}, {}'.format(dt.now().year, PROJECT_AUTHOR)
 PROJECT_URL = 'https://github.com/xlcnd/isbnlib'
 PROJECT_EMAIL = 'xlcnd@outlook.com'
+PROJECT_VERSION = __version__
 
 PROJECT_GITHUB_USERNAME = 'xlcnd'
 PROJECT_GITHUB_REPOSITORY = 'isbnlib'
 
-PYPI_URL = 'https://pypi.org/project/{}/'.format(PROJECT_PACKAGE_NAME)
 GITHUB_PATH = '{}/{}'.format(PROJECT_GITHUB_USERNAME,
                              PROJECT_GITHUB_REPOSITORY)
 GITHUB_URL = 'https://github.com/{}'.format(GITHUB_PATH)
 
-DOWNLOAD_URL = '{}/archive/{}.zip'.format(GITHUB_URL, "v" + __version__)
+DOWNLOAD_URL = '{}/archive/{}.zip'.format(GITHUB_URL, "v" + PROJECT_VERSION)
 PROJECT_URLS = {
     'Bug Reports': '{}/issues'.format(GITHUB_URL),
     'Dev Docs': 'https://isbnlib.readthedocs.io/en/latest/devs.html',
     'Forum': 'https://stackoverflow.com/questions/tagged/isbnlib',
     'License': PROJECT_LICENSE_URL,
 }
+
+PYPI_URL = 'https://pypi.org/project/{}/'.format(PROJECT_PACKAGE_NAME)
+PYPI_CLASSIFIERS = [
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+    'Operating System :: OS Independent',
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Topic :: Text Processing :: General',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+]
 
 PACKAGES = [
     'isbnlib',
@@ -56,7 +74,7 @@ PACKAGES = [
 
 setup(
     name=PROJECT_PACKAGE_NAME,
-    version=__version__,
+    version=PROJECT_VERSION,
     url=PROJECT_URL,
     download_url=DOWNLOAD_URL,
     project_urls=PROJECT_URLS,
@@ -69,22 +87,7 @@ setup(
     long_description=open('README.rst').read(),
     keywords=
     'ISBN metadata World_Catalogue Google_Books Open_Library BibTeX EndNote RefWorks MSWord opf BibJSON',
-    classifiers=[
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
-        'Operating System :: OS Independent',
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Topic :: Text Processing :: General',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ],
+    classifiers=PYPI_CLASSIFIERS,
     tests_require=['nose', 'coverage'],
     test_suite='nose.collector',
 )
