419ba23ae6ac4b932210a885d6affa911267ddb0
Alexandre Conde <alexandreconde@hotmail.com>
2018-10-20 12:08:11 +0100

    parametrize setup.py

--- setup.py
+++ setup.py
@@ -18,11 +18,9 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
 from datetime import datetime as dt
 from setuptools import setup
 from isbnlib import __version__
-
 
 PROJECT_NAME = 'isbnlib'
 PROJECT_PACKAGE_NAME = 'isbnlib'
@@ -37,8 +35,8 @@ PROJECT_GITHUB_USERNAME = 'xlcnd'
 PROJECT_GITHUB_REPOSITORY = 'isbnlib'
 
 PYPI_URL = 'https://pypi.org/project/{}/'.format(PROJECT_PACKAGE_NAME)
-GITHUB_PATH = '{}/{}'.format(
-    PROJECT_GITHUB_USERNAME, PROJECT_GITHUB_REPOSITORY)
+GITHUB_PATH = '{}/{}'.format(PROJECT_GITHUB_USERNAME,
+                             PROJECT_GITHUB_REPOSITORY)
 GITHUB_URL = 'https://github.com/{}'.format(GITHUB_PATH)
 
 DOWNLOAD_URL = '{}/archive/{}.zip'.format(GITHUB_URL, "v" + __version__)
@@ -49,8 +47,12 @@ PROJECT_URLS = {
     'License': PROJECT_LICENSE_URL,
 }
 
-PACKAGES = ['isbnlib', 'isbnlib/dev', 'isbnlib/_data', 'isbnlib/test', ]
-
+PACKAGES = [
+    'isbnlib',
+    'isbnlib/dev',
+    'isbnlib/_data',
+    'isbnlib/test',
+]
 
 setup(
     name=PROJECT_PACKAGE_NAME,
@@ -62,9 +64,11 @@ setup(
     author_email=PROJECT_EMAIL,
     packages=PACKAGES,
     license=PROJECT_LICENSE,
-    description='Extract, clean, transform, hyphenate and metadata for ISBNs (International Standard Book Number).',
+    description=
+    'Extract, clean, transform, hyphenate and metadata for ISBNs (International Standard Book Number).',
     long_description=open('README.rst').read(),
-    keywords='ISBN metadata World_Catalogue Google_Books Open_Library BibTeX EndNote RefWorks MSWord opf BibJSON',
+    keywords=
+    'ISBN metadata World_Catalogue Google_Books Open_Library BibTeX EndNote RefWorks MSWord opf BibJSON',
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
