513268010a11bd8ed030ce7ccc21229d6f55e5a5
Alexandre Conde <alexandreconde@hotmail.com>
2019-01-18 08:56:50 +0000

    update copyright

--- docs/conf.py
+++ docs/conf.py
@@ -56,7 +56,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'isbnlib'
-copyright = u'2014-2018, Alexandre Conde'
+copyright = u'2014-2019, Alexandre Conde'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
