b1f0d5805f613e1d1256ac4118724d7b1ef2ccf3
Alexandre Conde <alexandreconde@hotmail.com>
2019-02-18 12:39:26 +0000

    collect all config options

--- isbnlib/config.py
+++ isbnlib/config.py
@@ -3,24 +3,6 @@
 
 # --> Import only external modules! <--
 
-# Timeouts
-URLOPEN_TIMEOUT = 10  # seconds
-THREADS_TIMEOUT = 12  # seconds
-
-
-# URLOPEN_TIMEOUT is used by webservice
-def seturlopentimeout(seconds):  # pragma: no cover
-    """Set the value of URLOPEN_TIMEOUT (in seconds)."""
-    global URLOPEN_TIMEOUT
-    URLOPEN_TIMEOUT = seconds
-
-
-# THREADS_TIMEOUT is a parameter used downstream by Thread calls (see vias.py)
-def setthreadstimeout(seconds):  # pragma: no cover
-    """Set the value of THREADS_TIMEOUT (in seconds)."""
-    global THREADS_TIMEOUT
-    THREADS_TIMEOUT = seconds
-
 
 # API keys
 apikeys = {}
@@ -32,10 +14,12 @@ def add_apikey(service, apikey):  # pragma: no cover
     apikeys[service.lower()] = apikey
 
 
-# Generic Options
+# Options
 options = {
     'LOAD_FORMATTER_PLUGINS': True,
     'LOAD_METADATA_PLUGINS': True,
+    'THREADS_TIMEOUT': 12,  # seconds
+    'URLOPEN_TIMEOUT': 10,  # seconds
     'VIAS_MERGE': 'parallel',
 }
 
@@ -44,6 +28,27 @@ def set_option(option, value):  # pragma: no cover
     """Set the value for option."""
     global options
     options[option.upper()] = value
+
+
+# Timeouts
+URLOPEN_TIMEOUT = options.get('URLOPEN_TIMEOUT', 10)
+THREADS_TIMEOUT = options.get('THREADS_TIMEOUT', 12)
+
+
+# URLOPEN_TIMEOUT is used by webservice
+def seturlopentimeout(seconds):  # pragma: no cover
+    """Set the value of URLOPEN_TIMEOUT (in seconds)."""
+    global options, URLOPEN_TIMEOUT
+    set_option('URLOPEN_TIMEOUT', seconds)
+    URLOPEN_TIMEOUT = seconds
+
+
+# THREADS_TIMEOUT is a parameter used downstream by Thread calls (see vias.py)
+def setthreadstimeout(seconds):  # pragma: no cover
+    """Set the value of THREADS_TIMEOUT (in seconds)."""
+    global options, THREADS_TIMEOUT
+    set_option('THREADS_TIMEOUT', seconds)
+    THREADS_TIMEOUT = seconds
 
 
 LOAD_METADATA_PLUGINS = options.get('LOAD_METADATA_PLUGINS', True)
