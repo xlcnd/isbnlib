701ac613fdb741031771f2645998e887992a2fa1
Alexandre Conde <alexandreconde@hotmail.com>
2019-02-25 22:34:02 +0000

    add speed test

--- isbnlib/test/speed.py
+++ isbnlib/test/speed.py
@@ -5,9 +5,29 @@
 
 import time
 
-t = time.process_time()
-import isbnlib
-elapsed_time = time.process_time() - t
-millis = int(elapsed_time * 1000)
-print("{} milliseconds < 100 milliseconds".format(millis))
-assert elapsed_time < 100
+
+print("Test 'import speed' of:")
+
+
+def speed_isbnlib():
+    """Test import speed of 'isbnlib'."""
+    t = time.process_time()
+    import isbnlib
+    elapsed_time = time.process_time() - t
+    millis = int(elapsed_time * 1000)
+    print("(isbnlib)  {} milliseconds < 100 milliseconds".format(millis))
+    assert millis < 100
+
+
+def speed_registry():
+    """Test import speed of 'registry'."""
+    t = time.process_time()
+    from isbnlib import registry
+    elapsed_time = time.process_time() - t
+    millis = int(elapsed_time * 1000)
+    print("(registry) {} milliseconds < 100 milliseconds".format(millis))
+    assert millis < 100
+
+
+speed_isbnlib()
+speed_registry()
