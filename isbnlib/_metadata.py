72e87a015cb9c61f092b6f8224802345c380f461
Alexandre Conde <alexandreconde@hotmail.com>
2019-06-01 11:04:28 +0100

    apply decorator 'cache' to 'metadata'

--- isbnlib/_metadata.py
+++ isbnlib/_metadata.py
@@ -3,30 +3,23 @@
 
 from ._core import EAN13
 from ._exceptions import NotRecognizedServiceError, NotValidISBNError
+from .dev import cache
 
 
-def query(isbn, service='default', cache='default'):
+@cache
+def get_meta(isbn, service):
+    """Select the provider."""
+    from .registry import services
+    if service != 'default' and service not in services:  # pragma: no cover
+        raise NotRecognizedServiceError(service)
+    meta = services[service](isbn)
+    return meta if meta else {}
+
+
+def query(isbn, service='default'):
     """Query services like Google Books (JSON API), ... for metadata."""
-    # validate inputs
     ean = EAN13(isbn)
     if not ean:
         raise NotValidISBNError(isbn)
     isbn = ean
-    # only import when needed
-    from .registry import services
-    if service != 'default' and service not in services:  # pragma: no cover
-        raise NotRecognizedServiceError(service)
-    # set cache and get metadata
-    if cache is None:  # pragma: no cover
-        return services[service](isbn)
-    if cache == 'default':  # pragma: no cover
-        from .registry import metadata_cache
-        cache = metadata_cache
-    if cache is not None:
-        key = isbn + service
-        if key in cache:
-            return cache[key]
-    meta = services[service](isbn)
-    if meta and cache is not None:  # pragma: no cover
-        cache[key] = meta
-    return meta if meta else {}
+    return get_meta(ean, service)
--- isbnlib/test/test_metadata.py
+++ isbnlib/test/test_metadata.py
@@ -21,7 +21,7 @@ def test_query():
     assert_equals(len(repr(query('9780321534965', 'goob'))) > 100, True)
     #assert_equals(len(repr(query('9789934015960'))) > 100, True)
     assert_equals(len(repr(query(u'9781118241257'))) > 100, True)
-    assert_raises(Exception, query, '9780000000', 'goob', None)
+    assert_raises(Exception, query, '9780000000', 'goob')
     assert_raises(Exception, query, randrange(0, 1000000), 'goob')
 
 
@@ -30,6 +30,6 @@ def test_ext_meta():
     # test meta from core
     assert_equals(len(repr(meta('9780321534965', 'goob'))) > 100, True)
     assert_equals(len(repr(meta('9780321534965'))) > 100, True)
-    assert_raises(Exception, meta, '9780000000', 'goob', None)
+    assert_raises(Exception, meta, '9780000000', 'goob')
     assert_raises(Exception, meta, randrange(0, 1000000), 'goob')
-    assert_raises(Exception, meta, '9781849692343', 'goob', None)
+    assert_raises(Exception, meta, '9781849692343', 'goob')
