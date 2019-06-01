064942a78afdf6a31b6d83ad513a1ecf2d1b970f
Alexandre Conde <alexandreconde@hotmail.com>
2019-06-01 10:14:06 +0100

    add 'decorators'

---
+++ isbnlib/dev/_decorators.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""Decorator for isbnlib."""
+from functools import wraps
+
+
+def cache(func):
+    """Cache decorator."""
+    # noqa
+    @wraps(func)
+    def memoized_func(*args, **kwargs):
+        from ..registry import metadata_cache
+        cch = metadata_cache
+        if cch is None:
+            return func(*args, **kwargs)
+
+        key = str(func.__name__) + str(args) + str(kwargs)
+        if key in cch:
+            return cch[key]
+        else:
+            value = func(*args, **kwargs)
+            if value:
+                cch[key] = value
+            return value
+
+    return memoized_func
