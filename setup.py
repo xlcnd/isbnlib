4ab02bfe9254d0b79c052228357c5edc569671ec
Alexandre Conde <alexandreconde@hotmail.com>
2020-01-03 09:47:41 +0000

    add py38

--- setup.py
+++ setup.py
@@ -56,6 +56,7 @@ PYPI_CLASSIFIERS = [
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
     'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
     'Operating System :: OS Independent',
     'Development Status :: 5 - Production/Stable',
