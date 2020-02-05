2003421ef344746a4e0db675e2e85f5e06f8d431
Alexandre Conde <alexandreconde@hotmail.com>
2020-02-05 14:30:49 +0000

    add 'default' exceptions

---
+++ isbnlib/test/test_registry.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# flake8: noqa
+# pylint: skip-file
+
+from nose.tools import assert_equals, assert_raises
+
+from ..registry import setdefaultbibformatter, setdefaultservice
+
+
+# nose tests
+def test_setdefaultbibformatter():
+    """Test setdefaultbibformatter."""
+    assert_equals(setdefaultbibformatter('json'), None)
+    assert_raises(Exception, setdefaultbibformatter, 'default')
+    assert_raises(Exception, setdefaultbibformatter, '')
+    assert_raises(Exception, setdefaultbibformatter, 'xxx')
+
+
+def test_setdefaultservice():
+    """Test setdefaultservice."""
+    assert_equals(setdefaultservice('goob'), None)
+    assert_raises(Exception, setdefaultservice, 'default')
+    assert_raises(Exception, setdefaultservice, '')
+    assert_raises(Exception, setdefaultservice, 'xxx')
