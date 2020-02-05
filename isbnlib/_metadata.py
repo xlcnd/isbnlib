6c682e6c003c0616d572efecae6715fb2be42b4d
Alexandre Conde <alexandreconde@hotmail.com>
2020-02-05 18:11:03 +0000

    add logger

--- isbnlib/_metadata.py
+++ isbnlib/_metadata.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 """Query providers for metadata."""
 
+import logging
+
 from ._core import EAN13
 from ._exceptions import NotRecognizedServiceError, NotValidISBNError
 from .dev import cache
+
+LOGGER = logging.getLogger(__name__)
 
 
 @cache
@@ -17,6 +21,7 @@ def query(isbn, service='default'):
     # only import when needed
     from .registry import services
     if service != 'default' and service not in services:  # pragma: no cover
+        LOGGER.critical('%s is not a valid service', service)
         raise NotRecognizedServiceError(service)
     meta = services[service](isbn)
     return meta if meta else {}
