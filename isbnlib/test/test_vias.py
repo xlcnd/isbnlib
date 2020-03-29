cae5ce55e3f9d845058cdef770734f6e5a03d9fa
Alexandre Conde <alexandreconde@hotmail.com>
2020-03-29 15:00:07 +0100

    fix appveyor

--- isbnlib/test/test_vias.py
+++ isbnlib/test/test_vias.py
@@ -43,10 +43,10 @@ def test_vias_parallel():
 def test_vias_multi():
     """Test 'vias' (multi)."""
     # Is NOT allowed in Windows!
-    if os.getenv('APPVEYOR', '') != '':
-        return True
-    #if os.getenv('GITHUB_OS', '') == 'windows':
+    #if os.getenv('APPVEYOR', '') != '':
     #    return True
+    if os.getenv('APPVEYOR_OS', '') == 'windows':
+        return True
     if os.getenv('GITHUB_OS', '') == 'windows':
         return True
     named_tasks = (('task1', task1), ('task2', task2))
