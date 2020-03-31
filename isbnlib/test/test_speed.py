417ab5e8d17fa167d7ec69d8045bd32e9bb3b3e9
Alexandre Conde <alexandreconde@hotmail.com>
2020-03-31 18:14:15 +0100

    add 'speed' as a nose test

---
+++ isbnlib/test/test_speed.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# flake8: noqa
+# pylint: skip-file
+"""Crude Timer for 'import isbnlib'."""
+
+import time
+
+
+def test_speed_isbnlib():
+    """Test import speed of 'isbnlib'."""
+    t = time.process_time()
+    import isbnlib
+
+    elapsed_time = time.process_time() - t
+    millis = int(elapsed_time * 1000)
+    print('(isbnlib)  {} milliseconds < 100 milliseconds'.format(millis))
+    assert millis < 100
+    isbnlib.__version__
+
+
+def test_speed_registry():
+    """Test import speed of 'registry'."""
+    t = time.process_time()
+    from isbnlib import registry
+
+    elapsed_time = time.process_time() - t
+    millis = int(elapsed_time * 1000)
+    print('(registry) {} milliseconds < 135 milliseconds'.format(millis))
+    assert millis < 135
+    registry.BIBFORMATS
