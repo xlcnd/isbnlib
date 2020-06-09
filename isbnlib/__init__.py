6344bcfb2f84721ca0ee4ef8b0dcfe34900be563
Alexandre Conde <alexandreconde@hotmail.com>
2020-06-09 10:42:16 +0100

    reorder imports

--- isbnlib/__init__.py
+++ isbnlib/__init__.py
@@ -12,36 +12,36 @@ from ._exceptions import (
     ISBNLibException,
     NotRecognizedServiceError,
     NotValidISBNError,
-    PluginNotLoadedError,
     NotValidDefaultServiceError,
     NotValidDefaultFormatterError,
+    PluginNotLoadedError,
 )
 
 # main modules
 from ._core import (
-    is_isbn10,
-    is_isbn13,
-    to_isbn10,
-    to_isbn13,
     check_digit10,
     check_digit13,
     clean,
     canonical,
-    notisbn,
     get_isbnlike,
     get_canonical_isbn,
-    GTIN13,
+    is_isbn10,
+    is_isbn13,
+    notisbn,
+    to_isbn10,
+    to_isbn13,
     EAN13,
+    GTIN13,
     RE_ISBN10,
     RE_ISBN13,
     RE_LOOSE,
     RE_NORMAL,
     RE_STRICT,
 )
+from ._doitotex import doi2tex
 from ._ext import cover, desc, mask, meta, info, editions, isbn_from_words, doi, ren
 from ._goom import query as goom
 #from ._isbn import Isbn
-from ._doitotex import doi2tex
 from ._oclc import query_classify as classify
 
 # Ranges Database date
@@ -52,12 +52,12 @@ _nh = _logging.NullHandler()
 _logging.getLogger('isbnlib').addHandler(_nh)
 
 # alias
-ISBN13 = EAN13
 ean13 = EAN13
+ISBN13 = EAN13
 
 # dunders
 __all__ = (
-    #    'Isbn',
+    # 'Isbn',
     'is_isbn10',
     'is_isbn13',
     'clean',
@@ -92,9 +92,9 @@ __all__ = (
     'ISBNLibException',
     'NotRecognizedServiceError',
     'NotValidISBNError',
-    'PluginNotLoadedError',
     'NotValidDefaultFormatterError',
     'NotValidDefaultServiceError',
+    'PluginNotLoadedError',
 )
 
 __version__ = '3.10.4'
