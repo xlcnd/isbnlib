8be0d436de0931cf2b2901c12af527dec48be13b
Christian Clauss <cclauss@me.com>
2021-11-07 23:42:34 +0100

    pytest uses Python assert

--- isbnlib/test/test_webservice.py
+++ isbnlib/test/test_webservice.py
@@ -1,17 +1,10 @@
 # -*- coding: utf-8 -*-
 # flake8: noqa
 # pylint: skip-file
-"""
-nose tests
-"""
-
-from nose.tools import assert_equals
 
 from ..dev.webservice import query as wsquery
 
 
 def test_webservice():
     """Test that values can be passed to a WebService query."""
-    assert_equals(
-        len(repr(wsquery('http://example.org', values={'some': 'values'}))) > 0, True,
-    )
+    assert len(repr(wsquery('http://example.org', values={'some': 'values'})))
