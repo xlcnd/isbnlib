75a03b5554fdac3548f2e4d4a390ed6336660271
Christian Clauss <cclauss@me.com>
2021-11-08 09:41:51 +0100

    Fix isbnlib/test/test_webservice.py

--- isbnlib/test/test_webservice.py
+++ isbnlib/test/test_webservice.py
@@ -7,6 +7,4 @@ from ..dev.webservice import query as wsquery
 
 def test_webservice():
     """Test that values can be passed to a WebService query."""
-    assert_equals(
-        len(repr(wsquery('http://example.org', values={'some': 'values'}))) > 0, True,
-    )
+    assert len(repr(wsquery('http://example.org', values={'some': 'values'}))) > 0
