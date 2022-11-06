f7913bdf00bff96f9195578f77a98686f877aa76
Alexandre Conde <alexandreconde@hotmail.com>
2022-11-06 10:53:23 +0000

    fix escape sequences

--- isbnlib/test/test_unicode_to_utf8tex.py
+++ isbnlib/test/test_unicode_to_utf8tex.py
@@ -10,5 +10,5 @@ from ..dev._helpers import unicode_to_utf8tex
 
 def test_unicode_to_utf8tex():
     """Test 'unicode_to_utf8tex' identity transformation."""
-    assert unicode_to_utf8tex(u'\u00E2 \u00F5') == b'\^{a}\space \~{o}'
-    assert unicode_to_utf8tex(u'\u00E2 \u00F5', (b'\\space ',)) == b'\^{a} \~{o}'
+    assert unicode_to_utf8tex(u'\u00E2 \u00F5') == b'\\^{a}\\space \\~{o}'
+    assert unicode_to_utf8tex(u'\u00E2 \u00F5', (b'\\space ',)) == b'\\^{a} \\~{o}'
