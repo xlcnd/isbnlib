503393b9365f58dfb3e45094b71678d63861839b
Alexandre Conde <alexandreconde@hotmail.com>
2023-03-28 16:41:15 +0100

    drop py27 and clean code

--- setup.py
+++ setup.py
@@ -54,8 +54,6 @@ PROJECT_URLS = {
 PYPI_URL = 'https://pypi.org/project/{}/'.format(PROJECT_PACKAGE_NAME)
 PYPI_CLASSIFIERS = [
     'Programming Language :: Python',
-    'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
