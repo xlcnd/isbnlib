322f81ad84fa81e560a46d11fb79b709f261f547
Alexandre Conde <alexandreconde@hotmail.com>
2023-07-06 10:45:22 +0100

    delete old code

--- isbnlib/_goom.py
+++ isbnlib/_goom.py
@@ -2,11 +2,7 @@
 """Query the Google Books (JSON API v1) for metadata."""
 
 import logging
-
-try:
-    from urllib.parse import quote
-except ImportError:
-    from urllib import quote
+from urllib.parse import quote
 
 from .dev import cache, stdmeta
 from .dev._exceptions import NoDataForSelectorError, RecordMappingError
